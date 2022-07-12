78538a3634bb81bdd32f6ddedcb60bf8b2d10025
jlaguerre91 <jlaguerre91@gmail.com>
2022-07-11 23:19:49 -0400

    Updates

---
+++ precache-manifest.86357eb1af036aca1111de0f0f80212b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "404525a4e0698c01bf59",
+    "url": "/robofriends/static/css/main.fc8e8bae.chunk.css"
+  },
+  {
+    "revision": "404525a4e0698c01bf59",
+    "url": "/robofriends/static/js/main.404525a4.chunk.js"
+  },
+  {
+    "revision": "ce3ffd570b56f2a8ba48",
+    "url": "/robofriends/static/css/1.5c0442f2.chunk.css"
+  },
+  {
+    "revision": "ce3ffd570b56f2a8ba48",
+    "url": "/robofriends/static/js/1.ce3ffd57.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9f63f4ac6b2baaa39b8d7f70772717c3",
+    "url": "/robofriends/index.html"
+  }
+];
